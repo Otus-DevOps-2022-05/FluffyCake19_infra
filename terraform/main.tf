63f6836bc8a35f72ef7a68fc9374f5a573985ed8
FluffyCake19 <39598013+FluffyCake19@users.noreply.github.com>
2022-07-27 20:46:20 +0300

    fix path

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,17 +5,17 @@ provider "yandex" {
   zone                     = var.zone
 }
 module "vpc" {
-  source = "../modules/vpc"
+  source = "./modules/vpc"
 }
 module "app" {
-  source          = "../modules/app"
+  source          = "./modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = module.vpc.app_subnet_id
 }
 
 module "db" {
-  source          = "../modules/db"
+  source          = "./modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = module.vpc.app_subnet_id
